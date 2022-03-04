d74e293fa9c220383186348f6d6d57c2cb42112d
nkindi-bri <brigitte.uwinkindi@gmail.com>
2022-03-04 12:22:09 +0200

    first

---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func config() {
+	d, err := gorm.Open("mysql", "nkindi:nkindi22/simplerest?charset=utf8parseTime=True&loc=Local")
+
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
